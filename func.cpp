e0aa274ae6dd249a1d33bfbee6d8bf27dd23a673
Gaurav <57193590+gaurav884@users.noreply.github.com>
2022-07-17 21:22:32 +0530

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
    
-   int a = 5;
+   int a = 5;                      
    int b = 6;
    
-   cout<<"The result is "<<a+b<< " ";
+   cout<<"The result of addition of two numbers is given as "<<a+b<< " ";
 
     return 0;
 }
